3e44b70ca7042b0e6bcef3da8e3c5e8213b887a0
anissa guembre <guembreani@gmail.com>
2021-06-29 13:36:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return 'This one is on me!'
+  }
+  else if (ride> 200 && ride<=2500){
+    return 'I will gladly take your thirty bucks.'
+   }
+  else return 'No can do.'
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
+  if (x== "NYC"){
+    return "Ok, sounds good."
+  }
+  else return "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if (tip == "generous"){
+    return 'Thank you so much.'
+  }
+  else if (tip == "not as generous"){
+    return 'Thank you.'
+  }
+  else return "Bye."
   // Write your code here!
 }
